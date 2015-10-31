2d3d9515815b177272b6cd3cf9a179a0d5fd01b5
Brian Woodward <brian.woodward@gmail.com>
2015-10-31 12:24:45 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * npm-base <https://github.com/doowb/npm-base>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
